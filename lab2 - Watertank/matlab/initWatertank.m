09dc777335d11eba1015fee2284888d954f21f69
Luke <f.luky@gmx.at>
2024-01-19 13:59:44 +0100

    feat(initWatertank): System parameters and control parameters

--- lab2 - Watertank/matlab/initWatertank.m
+++ lab2 - Watertank/matlab/initWatertank.m
@@ -3,22 +3,39 @@ close all;
 clear;
 clc;
 
-%% System Parmeter
 
-data.SysPara.roh = 1000 %kg/m³
-data.SysPara.At = 50 %cm²
-data.SysPara.Aout = 25 %mm²
-data.SysPara.zeta = 0.05
-data.SysPara.g = 9.81 %m/s²
+%% System parmeter
+data.roh        = 1000; % kg/m³
+data.At         = 50;   % cm²
+data.Aout       = 25;   % mm²
+data.zeta       = 0.05;
+data.g          = 9.81; % m/s²
+data.initHeight = 0.1;  % cm
+data.a          = sqrt(2*data.g/(1+data.zeta))*data.Aout/data.At;
+
+
+%% Control paramters (fixed-point and trajectory)
+data.Controller.A = [0 1; 0 0];
+data.Controller.B = [0;1];
+data.Controller.C = [1 0];
+
+% Pole-locations for controller parameters
+% We want a second order critically damped system -> 2 poles
+% Therefore we need one double pole for critically damped
+poles = [complex(-0.2,0),complex(-0.2,0)];
+% We can use Ackerman, because system is stable (rank(ctrb(c.A,c.B)) == size(c.A,1))
+data.Controller.K = acker(data.Controller.A, data.Controller.B,poles);
+
+% calculate prefilter necessary for fixed point control
+data.Controller.N = -1/(data.Controller.C*inv(data.Controller.A-data.Controller.B*data.Controller.K)*data.Controller.B);
+
 
 %% Sensor input data
-
 v_out = [2.7,3,3.3,3.7,4];
 data.Sensor.v = [0.14,0.19,0.4,0.56,0.8]; % measured steady-state output voltage in V
 data.Sensor.h = [14,31,50,79,94]; % measured steady-state height in mm
 
 %% Fit
-
 data.Sensor.FitPara = polyfit(data.Sensor.v, data.Sensor.h,1)
 
 figure('Name', 'Waterlevel Sensor', 'NumberTitle', 'off');
